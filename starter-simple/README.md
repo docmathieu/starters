01d01fa1567e6b130891f06466629141caab9881
docm <doc.mathieu@gmail.com>
2019-03-22 10:52:37 +0100

    Starters - starter-simple
    starter-simple
    Angular 7.3.6 + Spring boot 2.1.3

---
+++ starter-simple/README.md
@@ -0,0 +1,2 @@
+# Starter
+Angular 7.3.6 + Spring boot 2.1.3
---
+++ starter-simple/src/main/java/fr/docm/startersimple/StarterSimpleApplication.java
@@ -0,0 +1,13 @@
+package fr.docm.startersimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterSimpleApplication.class, args);
+	}
+
+}
